cffe5e4177c4249a391e1350b66cc9f289d58c3e
yutajakchuntham-max <yutajakchuntham@gmail.com>
2026-01-23 14:26:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function openRegister() {
+  document.getElementById("registerPopup").style.display = "block";
+}
+
+function closeRegister() {
+  document.getElementById("registerPopup").style.display = "none";
+}
+function openMeetingPopup() {
+  document.getElementById("pdfPopup").style.display = "block";
+}
+
+function closeMeetingPopup() {
+  document.getElementById("pdfPopup").style.display = "none";
+}
+function openResultPopup() {
+  document.getElementById("resultPopup").style.display = "block";
+}
+
+function closeResultPopup() {
+  document.getElementById("resultPopup").style.display = "none";
+}
